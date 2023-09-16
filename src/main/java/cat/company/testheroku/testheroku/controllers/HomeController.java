f182fdfb5439526a252d45e2588935cd8d344076
Carles Company Soler <carlescs@gmail.com>
2023-09-16 15:54:42 +0200

    Add cooki on same domain.

--- src/main/java/cat/company/testheroku/testheroku/controllers/HomeController.java
+++ src/main/java/cat/company/testheroku/testheroku/controllers/HomeController.java
@@ -3,10 +3,14 @@ package cat.company.testheroku.testheroku.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
 @RestController("/")
 public class HomeController {
     @GetMapping("/")
-    public Person home() {
+    public Person home(HttpServletResponse response) {
+        response.addCookie(new Cookie("test", "test"));
         return new Person("John", "Doe");
     }    
 }
