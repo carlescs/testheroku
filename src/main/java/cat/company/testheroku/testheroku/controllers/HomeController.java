51362f905ca4a43816c212c63b392af4e2313237
Carles Company Soler <carlescs@gmail.com>
2023-09-17 14:51:02 +0200

    Add set cookie endpoint

--- src/main/java/cat/company/testheroku/testheroku/controllers/HomeController.java
+++ src/main/java/cat/company/testheroku/testheroku/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package cat.company.testheroku.testheroku.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.Cookie;
@@ -18,6 +20,16 @@ public class HomeController {
         response.addCookie(cookie);
         return new Person("John", "Doe");
     }    
+
+    @PostMapping("/set-cookie")
+    public Cookie setFromFrontend(@RequestBody NewCookie newCookie, HttpServletResponse response) {
+        Cookie cookie = new Cookie(newCookie.getName(), newCookie.getValue());
+        cookie.setDomain("company.cat");
+        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+        return cookie;
+    }
 
     @GetMapping("/test")
     public Cookie[] test(HttpServletRequest request) {
---
+++ src/main/java/cat/company/testheroku/testheroku/controllers/NewCookie.java
@@ -0,0 +1,13 @@
+package cat.company.testheroku.testheroku.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class NewCookie {
+    private String name;
+    private String value;
+}
