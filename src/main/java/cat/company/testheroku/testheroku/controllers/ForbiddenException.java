139d5fb5541fc653364c4214a6711f055dbacde8
Carles Company <carlescs@gmail.com>
2023-09-19 14:12:40 +0200

    Test image

---
+++ src/main/java/cat/company/testheroku/testheroku/controllers/ForbiddenException.java
@@ -0,0 +1,9 @@
+package cat.company.testheroku.testheroku.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException{
+
+}
--- src/main/java/cat/company/testheroku/testheroku/controllers/HomeController.java
+++ src/main/java/cat/company/testheroku/testheroku/controllers/HomeController.java
@@ -1,9 +1,16 @@
 package cat.company.testheroku.testheroku.controllers;
 
+import java.io.InputStream;
+import java.util.*;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.Cookie;
@@ -42,5 +49,17 @@ public class HomeController {
         Cookie[] cookies = request.getCookies();
 
         return cookies;
+    }
+
+    @GetMapping("/file")
+    @ResponseBody
+    public ResponseEntity<InputStreamResource> getFile(HttpServletRequest request){
+        if(!Arrays.stream(request.getCookies()).anyMatch(cookie->cookie.getName().equals("request")&&cookie.getValue().equals("secure")))
+            throw new ForbiddenException();
+        MediaType type=MediaType.IMAGE_PNG;
+        InputStream in=getClass().getResourceAsStream("/images/test.png");
+        return ResponseEntity.ok()
+            .contentType(type)
+            .body(new InputStreamResource(in));
     }
 }
